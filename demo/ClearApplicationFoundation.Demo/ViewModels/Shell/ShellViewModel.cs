9be29af19cbae45c7c2d41b2c79487d8832cf00f
Michael Gerfen <gerfen@hotmail.com>
2022-09-08 08:41:17 -0700

    Added code to display and "Application Loading" message while the application is starting.

--- demo/ClearApplicationFoundation.Demo/ViewModels/Shell/ShellViewModel.cs
+++ demo/ClearApplicationFoundation.Demo/ViewModels/Shell/ShellViewModel.cs
@@ -3,6 +3,10 @@ using Caliburn.Micro;
 using ClearApplicationFoundation.ViewModels.Infrastructure;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Navigation;
+using ClearApplicationFoundation.Demo.Views;
 
 namespace ClearApplicationFoundation.Demo.ViewModels.Shell
 {
@@ -12,7 +16,38 @@ namespace ClearApplicationFoundation.Demo.ViewModels.Shell
             base(navigationService, logger, eventAggregator, mediator, lifetimeScope)
         {
             Title = "Foundation Demo Application";
+            LoadingApplication = true;
+            NavigationService.Navigated += NavigationServiceOnNavigated;
         }
+
+      
+
+        private bool _loadingApplication;
+        public bool LoadingApplication
+        {
+            get => _loadingApplication;
+            set => Set(ref _loadingApplication, value);
+        }
+
+
+        private void NavigationServiceOnNavigated(object sender, NavigationEventArgs e)
+        {
+            var uri = e.Uri;
+
+            if (uri.OriginalString.Contains(nameof(HomeView)))
+            {
+                LoadingApplication = false;
+            }
+        }
+
+
+        protected override Task OnDeactivateAsync(bool close, CancellationToken cancellationToken)
+        {
+            NavigationService.Navigated -= NavigationServiceOnNavigated;
+            return base.OnDeactivateAsync(close, cancellationToken);
+        }
+
+     
 
     }
 }
