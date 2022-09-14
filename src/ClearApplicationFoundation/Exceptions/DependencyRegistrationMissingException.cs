4d1df95f478c5d1239ccab1239743e2131af9590
Michael Gerfen <gerfen@hotmail.com>
2022-09-14 13:22:07 -0700

    Added DependencyRegistrationMissingException

---
+++ src/ClearApplicationFoundation/Exceptions/DependencyRegistrationMissingException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ClearApplicationFoundation.Exceptions
+{
+    public class DependencyRegistrationMissingException : Exception
+    {
+        public DependencyRegistrationMissingException() : base("A unknown dependency has not been registered with the DI container.")
+        {
+
+        }
+        public DependencyRegistrationMissingException(string? message): base(message)
+        {
+            
+        }
+    }
+}
