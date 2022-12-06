f3355b5b38cc0c95233d1215ebfb20092de11bfa
Michael Gerfen <gerfen@hotmail.com>
2022-12-05 23:54:12 -0800

    Renamed ForEach to IndexedForEach

--- src/ClearApplicationFoundation/Extensions/EnumerableExtensions.cs
+++ src/ClearApplicationFoundation/Extensions/EnumerableExtensions.cs
@@ -5,7 +5,7 @@ namespace ClearApplicationFoundation.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T, int> action)
+        public static void IndexedForEach<T>(this IEnumerable<T> enumerable, Action<T, int> action)
         {
             var i = 0;
             foreach (var item in enumerable)
