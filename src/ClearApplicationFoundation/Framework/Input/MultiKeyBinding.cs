b2409cd57c30a369bacbf44b861c17e704c3e3d2
Michael Gerfen <gerfen@hotmail.com>
2023-02-17 11:55:14 -0800

    Moved input and command related classes into the "input" namespace

---
+++ src/ClearApplicationFoundation/Framework/Input/MultiKeyBinding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClearApplicationFoundation.Framework.Input
+{
+    public class MultiKeyBinding : InputBinding
+    {
+        [TypeConverter(typeof(MultiKeyGestureConverter))]
+        public override InputGesture? Gesture
+        {
+            get => base.Gesture as MultiKeyGesture;
+            set
+            {
+                if (!(value is MultiKeyGesture gesture))
+                {
+                    throw new ArgumentException();
+                }
+
+                base.Gesture = gesture;
+            }
+        }
+    }
+}
